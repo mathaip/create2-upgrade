d4d57691bbdbe1eb72b095fe30d5c829f341c3c1
Mathai Puthiakunnel <mathaiputhiakunnel@Mathais-MacBook-Air.local>
2020-03-25 16:45:22 -0300

    lab 6 changes

--- contracts/Springboard.sol
+++ contracts/Springboard.sol
@@ -11,20 +11,16 @@ contract Springboard {
   // scripts/bootstrap - contains the bytecodes breakdown
   // scripts/assemble.js - assemble the bootstrap bytecodes
   bytes public bootstrapCode = hex"6394198df1600052600060006004601c335afa80601b57600080fd5b3d600060203e6040516060f3";
-  
   event ContractCreated(address);
-  
   function execute(bytes memory runtimeCode) public payable {
-    require(!_mutex);
+    require(!_mutex, 'require mutex');
     _mutex = true;
       
     // Store the desired runtime bytecode
     _pendingRuntimeCode = runtimeCode;
-    
     bytes memory initCode = bootstrapCode;
     bytes32 salt = keccak256(abi.encodePacked(msg.sender));
     address addr;
-    
     // Create the new contract
     assembly {
       addr := create2(0, add(initCode, 0x20),
@@ -35,7 +31,6 @@ contract Springboard {
     emit ContractCreated(addr);
     _mutex = false;
   }
- 
 
   // This function is called by the new contract during the create2 call
   // from the bootstrap, fetching the desired bytecode
